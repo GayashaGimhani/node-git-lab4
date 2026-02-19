0d2d6aba6231742061cf32dbfa9e9b83778383b0
gayashagimhani <gayashagimhani12@gmail.com>
2026-02-19 12:44:11 +0530

    Added Promises

--- app.js
+++ app.js
@@ -36,3 +36,18 @@ https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
 
 const myModule = require('./my-module.js');
 console.log(myModule.myFunction());
+
+
+const condition = true;
+const myPromise = new Promise((resolve, reject) => {
+ if (condition) {
+ resolve('Success!');
+ } else {
+ reject('Failure!');
+ }
+});
+myPromise.then((result) => {
+ console.log(result);
+}).catch((error) => {
+ console.log(error);
+});
